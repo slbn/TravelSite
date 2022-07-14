8f3f4e3961fe52077dae2c808553a3f6caa7007d
slbn <silbenant@mail.ru>
2022-07-14 23:13:13 +0300

    Screen Adaptive

--- Js/main.js
+++ Js/main.js
@@ -8,3 +8,16 @@ window.onscroll = () => {
         }
 };
 }());   
+
+//burger handler
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector ('.header_nav');
+    const menuCloceItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+    menuCloceItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+    });
+}());
